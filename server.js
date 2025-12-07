024a0d51c2f7da801c9217a90ef5b346e2e71804
hesabischool1403-rgb <hesabi.school1403@gmail.com>
2025-12-07 23:42:22 +0330

    edit server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
   `);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // ==== روت تست موقت برای دیباگ ====
 app.get('/api/admin/test-enrollments', async (req, res) => {
